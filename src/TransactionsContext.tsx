9ceadd0600da33c462550d346de761cbc318a3c3
devspanholo <devspanholo@gmail.com>
2022-02-26 18:56:49 -0300

    Finzalizado as inserções

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -26,7 +26,7 @@ interface TransactionsProviderProps{
 
 interface TransactionsContextData{
     transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => void;
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext =  createContext<TransactionsContextData>(
@@ -43,9 +43,18 @@ export function TrasactionsProvider({children}: TransactionsProviderProps){
     },[]);
 
 
-    function createTransaction(transaction: TransactionInput){
+    async function createTransaction(transactionInput: TransactionInput){
 
-        api.post('/transactions', transaction)
+       const response =  await api.post('/transactions', {
+           ...transactionInput,
+           createdAt: new Date(),
+       })
+       const {transaction} = response.data;
+
+       setTransactions([
+           ...transactions,
+           transaction,
+       ]);
     }
 
     return(
